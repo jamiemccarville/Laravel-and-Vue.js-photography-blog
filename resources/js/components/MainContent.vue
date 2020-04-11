7af1408d401a8321d33712bf74d6be8cfbbe361b
Jamie McCarville <jamiemccarville@gmail.com>
2020-04-10 23:23:22 -0400

    Added and styled main post content

---
+++ resources/js/components/MainContent.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="post">
+    <h1 class="post__heading">10 Best Photo Apps For Incredible iPhone Photography (2020 Edition)</h1>
+    <!-- <img src="../assets/divider.svg" alt="page divider" class="post__divider" /> -->
+    <p class="post__p">
+      How do you capture more interesting travel photos with your iPhone? How do
+      you avoid taking the same cliché vacation photos that everyone else takes?
+      I recently interviewed Steffen Geldner – a talented iPhone photographer
+      with a passion for travel. In this article, Steffen reveals 7 tips for
+      shooting beautiful travel photos that will preserve the amazing memories
+      of your trip. Read on to discover how to take better travel photos with
+      your iPhone!
+    </p>
+    <!-- <img
+      src="../assets/plane-375.jpg"
+      :srcset="
+        `${photos.a} 1x, ${photos.b} 1.5x, ${photos.c} 2x, ${photos.d} 3x, ${photos.e} 4x`
+      "
+      alt="forced perspective grabbing an airplane"
+      class="post__img"
+    />-->
+    <h2
+      class="post__heading post__heading--h2"
+    >1. Research Your Destination To Find The Best Photo Opportunities</h2>
+    <section class="post__section">
+      <p class="post__p post__p--marginb">
+        Before your trip, I recommend researching the destination to find the
+        most interesting photo opportunities.
+      </p>
+      <p class="post__p post__p--marginb">
+        I always research the location I’m traveling to. I do this extensively
+        and
+        <br class="post__p--break" />obsessively!
+      </p>
+      <p class="post__p post__p--marginb">
+        It’s up to you how much research you do. But you should definitely spend
+        a bit of time looking up some great places to take pictures
+      </p>
+      <p class="post__p post__p--marginb">So, what kind of research can you do?</p>
+      <p class="post__p post__p--marginb">
+        First, you could search for photos that other people have taken at the
+        locations you’re planning to visit.
+      </p>
+      <p class="post__p post__p--marginb">
+        Search on Instagram or Google to get an idea of the kinds of photos
+        people take there.
+      </p>
+    </section>
+    <h2
+      class="post__heading post__heading--h2 post__heading--h2--2"
+    >2. Capture Your Journey To Tell The Complete Story Of Your Travels</h2>
+    <section class="post__section">
+      <p
+        class="post__p post__p--marginb"
+      >Travel photography isn’t just about photographing the destination.</p>
+      <p class="post__p post__p--marginb">
+        For a more complete photographic story, capture your journey to and from
+        the location you’re visiting.
+      </p>
+      <p class="post__p post__p--marginb">
+        If you’re traveling by plane, try to get a window seat where you have a
+        view of
+        <br class="post__p--break" />the wing or propeller.
+      </p>
+      <p class="post__p post__p--marginb">
+        And of course, you could capture a beautiful aerial view of clouds,
+        mountains, or city lights as you’re flying above.
+      </p>
+      <p class="post__p post__p--marginb">
+        Your journey will often involve several different types of transport.
+        And they all offer great photo opportunities.
+      </p>
+      <p class="post__p post__p--marginb">
+        Whether you’re traveling by car, taxi, bus, train, boat, or plane, try
+        to capture
+        <br class="post__p--break" />some interesting photos.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+// let photos = {
+//   a: require("../assets/plane-375.jpg"),
+//   b: require("../assets/plane-560.jpg"),
+//   c: require("../assets/plane-750.jpg"),
+//   d: require("../assets/plane-1120.jpg"),
+//   e: require("../assets/plane-1680.jpg")
+// };
+export default {
+  computed: {
+    photos() {
+      return photos;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/views/BlogArticle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <main class="container">
+      <main-content></main-content>
+      <aside-content></aside-content>
+    </main>
+  </div>
+</template>
+
+<script>
+import MainContent from "../components/MainContent";
+import AsideContent from "../components/AsideContent";
+export default {
+  name: "App",
+  components: {
+    MainContent,
+    AsideContent
+  },
+  data() {
+    return {
+      showPopup: false
+    };
+  }
+};
+</script>
+
+<style></style>
