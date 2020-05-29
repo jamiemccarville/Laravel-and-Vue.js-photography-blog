ee5214b0bf1430fdc61d7d5f56d76647ada0e4ff
Jamie McCarville <jamiemccarville@gmail.com>
2020-05-28 23:10:55 -0400

    added forceScheme('https') to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,21 +8,16 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
-        //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
-        //
+        \URL::forceScheme('https');
     }
 }
