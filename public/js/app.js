7e96bcb044273d62951b53f0ccd8ed807db5962f
Jamie McCarville <jamiemccarville@gmail.com>
2020-05-28 23:31:46 -0400

    commented out forceScheme('https'):

--- public/js/app.js
+++ public/js/app.js
@@ -13788,14 +13788,15 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/app/app.scss ***!
+  \**************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/jamiemccarville/coding/projects/ips-dev-test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/jamiemccarville/coding/projects/ips-dev-test/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/jamiemccarville/coding/projects/011-iPS/ips-dev-test/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/jamiemccarville/coding/projects/011-iPS/ips-dev-test/resources/sass/app.scss */"./resources/sass/app.scss");
+!(function webpackMissingModule() { var e = new Error("Cannot find module '/Users/jamiemccarville/coding/projects/011-iPS/ips-dev-test/resources/app/app.scss'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 
 
 /***/ })
